ebb0eacd21be07c8a83ab408cfae143be3ea55e7
adel <adel.khedri.mirqaid@gmail.com>
2024-04-15 04:00:08 -0700

    login sinup full view

---
+++ user/forms.py
@@ -0,0 +1,46 @@
+from typing import Any
+from django import forms
+from . import validators
+from .models import User
+from django.utils.translation import gettext_lazy as _
+
+def_atter = { 'class': 'form-control background text-white' }
+
+
+class UserLoginForm(forms.Form):
+    phone_number = forms.IntegerField(help_text='09123456789', widget=forms.NumberInput(attrs=def_atter), validators=[validators.phone_number_exist, ])
+    password = forms.CharField(help_text="*******", widget=forms.PasswordInput(attrs=def_atter), validators=[validators.password_validator])
+
+
+class UserCreateForm(forms.ModelForm):
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs=def_atter), validators=[validators.password_validator])
+
+    class Meta:
+        model = User
+        fields = ['username', 'phone_number', 'password', 'password2']
+
+        widgets = {
+            'username': forms.TextInput(attrs=def_atter),
+            'phone_number': forms.NumberInput(attrs=def_atter),
+            'password': forms.PasswordInput(attrs=def_atter),
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'این فیلد اجباری است',
+                'unique': 'این یوزرنیم تکراری است',
+            },
+            'phone_number': {
+                'required': 'این فیلد اجباری است',
+                'unique': 'این شماره تکراری است',
+            }
+        }
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password2 = cleaned_data.get('password2', None)
+        password1 = cleaned_data['password']
+        if password1 != password2:
+            raise forms.ValidationError(_('رمز عبور نباید با هم تفاوت داشته باشند'))
+        return cleaned_data
--- user/views.py
+++ user/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import User, Profile
+from .forms import UserLoginForm, UserCreateForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+class LoginView(View):
+    template_name = 'user/login.html'
+
+    def get(self, request, *args, **kwargs):
+        userForm = UserLoginForm()
+        return render(request, self.template_name, {'form': userForm})
+    
+    def post(self, request, *args, **kwargs):
+        userForm = UserLoginForm(request.POST)
+        
+        if userForm.is_valid():
+            phone_number = userForm.cleaned_data['phone_number']
+            password = userForm.cleaned_data['password']
+            user = authenticate(request, phone_number=phone_number, password=password)
+            print(user)
+            if user is not None:
+                login(request, user=user)
+                return redirect('/admin/')
+        
+        return render(request, self.template_name, {'form': userForm})
+
+
+class SinUpView(View):
+    template_name = 'user/sinup.html'
+    
+    def get(self, request, *args, **kwargs):
+        userForm = UserCreateForm()
+        return render(request, self.template_name, {'form': userForm})
+    
+    def post(self, request, *args, **kwargs):
+        userForm = UserCreateForm(request.POST)
+        if userForm.is_valid():
+            userForm.save()
+            userForm = UserCreateForm()
+        return render(request, self.template_name, {'form': userForm})
